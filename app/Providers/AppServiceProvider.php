4e29e3846a0bfa92c458bd354ffd65e51ec02b91
DewandraR <91871312+DewandraR@users.noreply.github.com>
2025-09-24 15:13:22 +0700

    APPService (http)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- [1] TAMBAHKAN BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // [2] TAMBAHKAN BLOK KODE INI
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            URL::forceScheme('http');
+        }
     }
 }
